31b94320a586b9e35b594150a141927206d54f26
Krause <matheus.krause@inf.ufrgs.br>
2020-07-05 12:30:12 -0300

    Adds Rail Fence Cipher solution

---
+++ CodeWars/Challenges3Kyu/RailFenceCipher/RailFenceCipher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars.Challenges3Kyu.RailFenceCipher
+{
+    public class RailFenceCipher
+    {
+        static int[] GenerateSeq(string s, int n)
+        {
+            var d = n * 2 - 2;
+            var buffer = new int[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var iv = i % d;
+                var v = iv < n ? iv : n - 2 - (iv % n);
+                
+                buffer[i] = v;
+            }
+
+            return buffer;
+        }
+        
+        public static string Encode(string s, int n)
+        {
+            var seq = GenerateSeq(s,n);
+
+            return new string(s.Select((s,i) => (s,i)).OrderBy(c => seq[c.i]).Select(c => c.s).ToArray());
+        }
+
+        public static string Decode(string s, int n)
+        {
+            var seq = GenerateSeq(s, n); 
+
+            var decSeq = seq.Select((v, i) => (v, i)).OrderBy(e => e.v).Select(e => e.i).ToArray();
+
+            Span<char> decS = stackalloc char[s.Length];
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                decS[decSeq[i]] = s[i];
+            }
+            
+            return new string(decS);
+        }
+    }
+}
---
+++ CodeWars/Challenges3Kyu/RailFenceCipher/RailFenceCipherTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using CodeWars.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CodeWars.Challenges3Kyu.RailFenceCipher
+{
+    public class RailFenceCipherTests
+    {
+        public RailFenceCipherTests(ITestOutputHelper helper)
+        {
+            Console.SetOut(new TestOutputWriter(helper));
+        }
+        
+        [Fact]
+        public void EncodeSampleTests()
+        {
+            string[][] encodes = 
+            {
+                new []{ "WEAREDISCOVEREDFLEEATONCE", "WDEEFARLREEEVEEDOACICTNSO"},
+                new[] { "WEAREDISCOVEREDFLEEATONCE", "WECRLTEERDSOEEFEAOCAIVDEN" },  // 3 rails
+                new[] { "Hello, World!", "Hoo!el,Wrdl l" },    // 3 rails
+                new[] { "Hello, World!", "H !e,Wdloollr" },    // 4 rails
+                new[] { "", "" }                               // 3 rails (even if...)
+            };
+            int[] rails = { 8, 3, 3, 4, 3 };
+            for (int i = 0; i < encodes.Length; i++)
+            {
+                Assert.Equal(encodes[i][1], RailFenceCipher.Encode(encodes[i][0], rails[i]));
+            }
+        }
+
+        [Fact]
+        public void DecodeSampleTests()
+        {
+            string[][] decodes =
+            {
+                new []{ "WECRLTEERDSOEEFEAOCAIVDEN", "WLSADOOTEEECEAEECRFINVEDR" },
+                new[] { "WIREEEDSEEEACAECVDLTNROFO","WEAREDISCOVEREDFLEEATONCE"},
+                new[] { "WECRLTEERDSOEEFEAOCAIVDEN", "WEAREDISCOVEREDFLEEATONCE" },    // 3 rails
+                new[] { "H !e,Wdloollr", "Hello, World!" },    // 4 rails
+                new[] { "", "" }                               // 3 rails (even if...)
+            };
+            int[] rails =
+            {
+                5,
+                4,
+                3, 
+                4, 
+                3
+            };
+            for (int i = 0; i < decodes.Length; i++)
+            {
+                Assert.Equal(decodes[i][1], RailFenceCipher.Decode(decodes[i][0], rails[i]));
+            }
+        }
+    }
+}
